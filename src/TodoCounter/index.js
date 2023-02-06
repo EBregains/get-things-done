5475461d2d1c98e6f51374a6ea891e4f7c322f78
EBregains <ebregains@gmail.com>
2023-02-06 03:50:12 -0500

    added icons to the counter

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -5,6 +5,19 @@ import './TodoCounter.css';
 
 function TodoCounter() {
 
+  const setCounterIcon = (total, done) => {
+    if (done !== 0) {
+      let ratio = total / done;
+      if ( ratio === 1) return '😎';
+      else if ( ratio <= 1.3) return '🙂'
+      else if ( ratio <= 2) return '😰';
+      else if ( ratio <= 3) return '🥵';
+      else return '🫠';
+    } else if (total !== 0) {
+      return '🥱';
+    } else return '😶';
+  }
+
   const { numCompletedTodos, numTotalTodos } = React.useContext(TodoContext)
   return (
     <section className="TodoCounter">
@@ -13,7 +26,10 @@ function TodoCounter() {
         <span className="-pink">THINGS</span> 
         <span className="-red">DONE.</span>
       </h1>
-      <h2 className="TodoCounter-counter">You completed<span>{numCompletedTodos} out of {numTotalTodos}</span></h2>
+      <section className="TodoCounter">
+        <h2 className="TodoCounter-counter">You completed<span>{numCompletedTodos} out of {numTotalTodos}</span></h2>
+        <p className="TodoCounter-counter-icon">{setCounterIcon(numTotalTodos, numCompletedTodos)}</p>
+      </section>
     </section>
   )
 }
